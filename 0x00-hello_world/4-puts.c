759a676bbbf3dff043270db0a11c43163a1ff387
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-06-15 13:56:46 +0000

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+
+
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
