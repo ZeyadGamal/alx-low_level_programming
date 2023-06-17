eb2c2a4719303737fb669415a763c8979d87e7ea
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-06-17 19:43:55 +0000

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+/**
+ * main - Entrance
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (C = 'A'; C <= 'Z'; C++)
+		putchar(C);
+	putchar('\n');
+
+	return (0);
+}
