8d7df594771b7a72bf4c2fd68ba6e6d60ee8f80b
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-06-20 20:27:05 +0000

    mandatory tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_islower - check if character is lower case
+ * @c: characted to checked
+ * Return: 1 if lowercase, else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
