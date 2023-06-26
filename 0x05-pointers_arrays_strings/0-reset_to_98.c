01001038644834db3e3e4aeec6a4600cc0c399f6
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-06-26 16:35:19 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer as a parameter
+ * and updates the valute it points to
+ * @n: input
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
