fe6d9cbbe74299a97f6f1f4dc33f442f0399bf88
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-06-29 14:47:41 +0000

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: pointer to destination
+ * @src: pointer to source
+ * @n: number of bytes
+ * Return: pointer to result string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] == '\0')
+			break;
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
