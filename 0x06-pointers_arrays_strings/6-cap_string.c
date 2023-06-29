1d2534cd2f0ea707adc440c4ed6e99653a3ba2e8
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-06-29 17:50:58 +0000

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,42 @@
 #include "main.h"
-
-
 /**
  * cap_string - capitalizes all words of a string
- * @s: string
- * Return: resulting string
+ * @s: input value
+ *
+ *
+ * Return: string
  */
-
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (s[i])
+	i = 0;
+	if (s[0] >= 'a' && s[0] <= 'z')
 	{
-		while (!(s[i] >= 'a' && s[i] <= 'z'))
-			i++;
-
-		if (s[i - 1] == ' ' ||
-		    s[i - 1] == '\t' ||
-		    s[i - 1] == '\n' ||
-		    s[i - 1] == ',' ||
-		    s[i - 1] == ';' ||
-		    s[i - 1] == '.' ||
-		    s[i - 1] == '!' ||
-		    s[i - 1] == '?' ||
-		    s[i - 1] == '"' ||
-		    s[i - 1] == '(' ||
-		    s[i - 1] == ')' ||
-		    s[i - 1] == '{' ||
-		    s[i - 1] == '}' ||
-		    i == 0)
-			s[i] -= 32;
-
-		i++;
+		s[0] = s[0] - 32;
 	}
-
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		switch (s[i])
+		{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '\n':
+			case '\t':
+				if (s[i + 1] > 96 && s[i + 1] < 123)
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+		}
+	}
 	return (s);
-
 }
