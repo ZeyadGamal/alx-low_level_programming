c509f70ba98e169fca00a665c1b35ac9126f927f
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-07-03 10:17:55 +0000

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+/**
+ * _strstr - find first ocurrence of substring needle in string haystack
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to beginning of located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
