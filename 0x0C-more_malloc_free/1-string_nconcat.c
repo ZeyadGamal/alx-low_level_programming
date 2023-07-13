23769d35940bb53e4f24ce1433dac3252c336ab1
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-07-13 09:45:26 +0000

    task 1 changes v3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,11 +37,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	while (n < len2 && i < (len1 + n))
 	{
-		str[i++] = s2[j++];
+		str[i] = s2[j];
+		i++;
+		j++;
 	}
 	while (n >= len2 && i < (len1 + len2))
 	{
-		str[i++] = s2[j++];
+		str[i] = s2[j];
+		i++;
+		j++;
 	}
 	str[i] = '\0';
 
