1f636bd08588cfd01ffd33d6f3db1ed873d1e50d
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-07-17 12:28:56 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: dogs's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
