34d22fbfafcabfd4d15670b97c5e29364a655613
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-07-17 12:53:08 +0000

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,6 @@
  * @age: dog's age
  * @owner: dog's owner name
  */
-
 struct dog
 {
 	char *name;
@@ -18,7 +17,6 @@ struct dog
 /**
  * dog_t - new name for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
