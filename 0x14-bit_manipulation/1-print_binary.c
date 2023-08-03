f75e6cf9f0565eae1cf2224ab2cf21565bb78df1
Zeyad Gamal <zeyad.gamal@hotmail.com.com>
2023-08-03 22:08:17 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary number
+ * @n: number
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int currN;
+	int j = 0;
+	int c = 0;
+
+	for (j = 63; j >= 0; j--)
+	{
+		currN = n >> j;
+
+		if (currN & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c)
+			_putchar('0');
+	}
+	if (!c)
+		_putchar('0');
+}
